25d1d88a87122b8a7ecdfe9321a545b7ce519d88
Dsumanth11 <sumanth.bittu100@gmail.com>
2023-02-26 11:02:56 +0530

    done adding color

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const app=new express();
+app.use(express.static('frontend'));
+
 app.get('/',(req,res)=>{
-    res.send("Hello World");
+    res.sendFile(__dirname+"/frontend/index.html");
 });
 app.get('/myname',(req,res)=>{
     res.send("This is sumanth");
-})
+});
 app.listen(3000,function(){
     console.log("Server running on http://localhost:3000/");
 });
