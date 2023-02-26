0601d79c95924de6478063257c624fd191c1a4b4
Dsumanth11 <sumanth.bittu100@gmail.com>
2023-02-26 10:09:17 +0530

    done

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const app=new express();
 app.get('/',(req,res)=>{
     res.send("Hello World");
 });
+app.get('/myname',(req,res)=>{
+    res.send("This is sumanth");
+})
 app.listen(3000,function(){
     console.log("Server running on http://localhost:3000/");
 });
